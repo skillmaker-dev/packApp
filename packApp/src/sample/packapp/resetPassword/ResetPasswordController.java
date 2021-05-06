8d76313cfc9e2f6d0fcb54032277a59bafb2eda7
OussamaBouchentouf <82152041+OussamaBouchentouf@users.noreply.github.com>
2021-05-06 22:19:02 +0000

    fix some bugs

--- packApp/src/sample/packapp/resetPassword/ResetPasswordController.java
+++ packApp/src/sample/packapp/resetPassword/ResetPasswordController.java
@@ -3,12 +3,10 @@ package sample.packapp.resetPassword;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -19,15 +17,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-public class ResetPasswordController extends rootPageController{
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
+public class ResetPasswordController extends rootPageController {
 
     @FXML
     private TextField emailInput;
     @FXML
-    private Label myLabel50;
 
     private String email;
 
@@ -42,7 +36,15 @@ public class ResetPasswordController extends rootPageController{
                 email =resultSet.getString("email");
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Information!");
+            alert.setHeaderText("Connection Failed!!");
+            alert.setContentText("Click Ok to try again");
+            alert.show();
+            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+            Image myIcone = new Image("sample/icon/iconfinder_sign-error_299045.png");
+            stage.getIcons().add(myIcone);
+            return "ConnectionFailed";
         }
 
         return email;
@@ -50,33 +52,43 @@ public class ResetPasswordController extends rootPageController{
 
     public void check(ActionEvent event) throws IOException {
 
-        if(emailInput.getText().equals(displayEmail())){
+        if(!displayEmail().equals("ConnectionFailed")){
+            if(emailInput.getText().equals(displayEmail())){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information!");
+                alert.setHeaderText("Your Password Is : " + displayCurrentpassword());
+                alert.setContentText("Click Ok");
+                alert.show();
+                Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+                Image myIcone = new Image("sample/icon/iconfinder_Info_728979.png");
+                stage.getIcons().add(myIcone);
 
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Information!");
-            alert.setHeaderText("Your Password Is : " + displayCurrentpassword());
-            alert.setContentText("Click Ok");
-            alert.show();
-            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-            Image myIcone = new Image("sample/icon/iconfinder_Info_728979.png");
-            stage.getIcons().add(myIcone);
-
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../loginPage/rootPage.fxml"));
-            root = loader.load();
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        }else{
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("ERROR!");
-            alert.setHeaderText("Email does not exist!!");
-            alert.setContentText("Click Ok to Try Again");
-            alert.show();
-            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-            Image myIcone = new Image("sample/icon/iconfinder_sign-error_299045.png");
-            stage.getIcons().add(myIcone);
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../loginPage/rootPage.fxml"));
+                Parent root = loader.load();
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("ERROR!");
+                alert.setHeaderText("Email does not exist!!");
+                alert.setContentText("Click Ok to Try Again");
+                alert.show();
+                Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+                Image myIcone = new Image("sample/icon/iconfinder_sign-error_299045.png");
+                stage.getIcons().add(myIcone);
+            }
         }
+
     }
 
+    public void home(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../loginPage/rootPage.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
