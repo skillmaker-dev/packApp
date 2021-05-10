c14acbff1c1a40642cddef23d561f3d4f6ae04ee
Anas Chahid <anas.chahid777@gmail.com>
2021-05-10 16:24:20 +0000

    added Reference Generator

--- packApp/src/sample/packapp/mainPage/mainPageController.java
+++ packApp/src/sample/packapp/mainPage/mainPageController.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class mainPageController {
@@ -22,6 +23,8 @@ public class mainPageController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+
     }
     public void nouvelleCommande(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../nouvelleCommande/nouvelleCommande.fxml"));
