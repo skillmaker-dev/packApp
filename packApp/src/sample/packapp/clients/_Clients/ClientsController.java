6036b5ad6030e057e07b88593bb117e44d9f34af
OussamaBouchentouf <82152041+OussamaBouchentouf@users.noreply.github.com>
2021-05-19 15:10:34 +0100

    update 2

--- packApp/src/sample/packapp/clients/_Clients/ClientsController.java
+++ packApp/src/sample/packapp/clients/_Clients/ClientsController.java
@@ -66,7 +66,7 @@ public class ClientsController implements Initializable {
         parentContainer.getChildren().add(root);
         Timeline timeline = new Timeline();
         KeyValue kv = new KeyValue(root.translateXProperty(),0, Interpolator.EASE_IN);
-        KeyFrame kf = new KeyFrame(Duration.seconds(1),kv);
+        KeyFrame kf = new KeyFrame(Duration.seconds(0.5),kv);
         timeline.getKeyFrames().add(kf);
         timeline.setOnFinished(event1 -> {
             parentContainer.getChildren().remove(container);
